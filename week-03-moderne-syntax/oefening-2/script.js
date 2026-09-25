90bba4e067d77954ffafd228f0856498fc4462d4
TygoSebel <9028035@student.zadkine.nl>
2026-09-25 12:53:16 +0200

    oefening-2afgerond

--- week-03-moderne-syntax/oefening-2/script.js
+++ week-03-moderne-syntax/oefening-2/script.js
@@ -1,4 +1,27 @@
-// Voeg een event listener toe aan elke knop
-// Knop 1: voeg tekst toe aan #message
-// Knop 2: voeg een <li> toe aan #list met een tekst
-// Knop 3: wissel de klasse 'active' op #message
+let btn1 = document.getElementById('btn-1');
+let btn2 = document.getElementById('btn-2');
+let btn3 = document.getElementById('btn-3');
+let message = document.getElementById('message');
+let list = document.getElementById('list');
+
+
+btn1.addEventListener('click', () => {
+    message.textContent = 'Je hebt op knop 1 geklikt!';
+});
+
+
+btn2.addEventListener('click', () => {
+    message.textContent = 'Je hebt op knop 2 geklikt!';
+
+    let item = document.createElement('li');
+    item.textContent = 'Nieuw item toegevoegd!';
+
+    list.appendChild(item);
+});
+
+
+btn3.addEventListener('click', () => {
+    message.textContent = 'Je hebt op knop 3 geklikt!';
+
+    message.classList.toggle('active');
+});
